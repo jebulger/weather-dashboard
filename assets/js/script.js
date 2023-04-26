4257c7058fff3fd640d822ba9b859b7d2b1685ae
Jack <jebulger1003@gmail.com>
2023-04-26 13:18:06 -0400

    app is now fully functional, all that is left is styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var searchButton = document.getElementById('search-button');
 var userInput = document.getElementById('user-input')
-var searchHistory = document.getElementById('search-history');
+var previousSearches = document.getElementById('search-history');
 
-// Id selectors for the elements that make up the card of the current day's weather
+// Id selectors for the elements of the current weather card
 var currentCityName = document.getElementById('current-city');
 var currentCityIcon = document.getElementById('current-symbol');
 var currentCityTemp = document.getElementById('temp');
@@ -50,17 +50,46 @@ var lon;
 var cityForecast = {}
 var weatherToday = {}
 
-var searchHistory = localStorage.getItem('city');
+// Rendering search history upon page load 
+historyToList();
 
-// if (searchHistory !== undefined) {
-//     var 
-// }
+// This function grabs the search history and appends them as clickable buttons onto the page
+// Upon loading the page this function is called and will list any previous searches
+// Upon clicking one of the search buttons, the textcontent of the button clicked is then
+// used to perform an alternative search
+function historyToList() {
+    var loadedSearchHistory = localStorage.getItem('city');
+    // Removing the child buttons each time this is called to prevent
+    // the list from compounding, then reappending 
+    while (previousSearches.firstChild) {
+        previousSearches.removeChild(previousSearches.firstChild);
+    }
+    if (loadedSearchHistory) {
+        loadedSearchHistory = JSON.parse(loadedSearchHistory);
+        console.log(loadedSearchHistory);
+    } else {
+        loadedSearchHistory = [];
+    }
+    if (loadedSearchHistory.length > 0) {
+        for (var i = 0; i < loadedSearchHistory.length; i++) {
+            var historyButton = document.createElement('button');
+            previousSearches.appendChild(historyButton);
+            historyButton.textContent = loadedSearchHistory[i];
+            historyButton.addEventListener('click', function (event) {
+                var searchValue = event.target.textContent;
+                altSearch(searchValue);
+            });
+        }
+    }
+}
 
-// function localStorageToPage() {
-//     for (i = 0; i < searchHistory.length; i++) {
-
-//     }
-// }
+// Function to perform an alternate search when the search history buttons are selected
+// Skips over the submit search and
+function altSearch(searchValue) {
+    console.log(searchValue);
+    chosenCity = searchValue;
+    getCoordinates();
+}
 
 // Checks to make sure a city was entered and begins to get the coordinates if so
 function submitSearch(event) {
@@ -71,7 +100,14 @@ function submitSearch(event) {
         location.reload();
     } else {
         console.log(chosenCity);
-        localStorage.setItem('city', chosenCity);
+        var searchHistory = localStorage.getItem('city');
+        if (searchHistory) {
+            searchHistory = JSON.parse(searchHistory);
+        } else {
+            searchHistory = [];
+        }
+        searchHistory.push(chosenCity);
+        localStorage.setItem('city', JSON.stringify(searchHistory));
         console.log(localStorage);
         getCoordinates();
     }
@@ -124,8 +160,10 @@ function getWeatherApi() {
 // Places the weather data for the selected city onto the page
 function renderDataToPage() {
     var pngUrl = 'https://openweathermap.org/img/wn/';
-
+    
+    var unixTime = weatherToday.dt;
     currentCityName.textContent = weatherToday.name;
+    currentCityName.textContent = currentCityName.textContent.concat(' (' + unixConversion(unixTime) + ')');
     var currentCityIconCode = weatherToday.weather[0].icon;
     var currentCityIconUrl = pngUrl + currentCityIconCode + '@2x.png';
     currentCityIcon.src = currentCityIconUrl;
@@ -133,55 +171,56 @@ function renderDataToPage() {
     currentCityWind.textContent = 'Wind: ' + weatherToday.wind.speed + ' MPH';
     currentCityHumidity.textContent = 'Humidity: ' + weatherToday.main.humidity + ' %';
 
-    var unixTime = cityForecast.list[4].dt;
+    unixTime = cityForecast.list[6].dt;
     forecast1Date.textContent = unixConversion(unixTime);
-    var day1IconCode = cityForecast.list[4].weather[0].icon;
+    var day1IconCode = cityForecast.list[6].weather[0].icon;
     var day1IconUrl = pngUrl + day1IconCode + '@2x.png';
     forecast1Symbol.src = day1IconUrl;
-    forecast1Temp.textContent = 'Temp: ' + cityForecast.list[4].main.temp + ' °F';
-    forecast1Wind.textContent = 'Wind: ' + cityForecast.list[4].wind.speed + ' MPH';
-    forecast1Humidity.textContent = 'Humidity: ' + cityForecast.list[4].main.humidity + ' %';
+    forecast1Temp.textContent = 'Temp: ' + cityForecast.list[6].main.temp + ' °F';
+    forecast1Wind.textContent = 'Wind: ' + cityForecast.list[6].wind.speed + ' MPH';
+    forecast1Humidity.textContent = 'Humidity: ' + cityForecast.list[6].main.humidity + ' %';
 
-    unixTime = cityForecast.list[12].dt;
+    unixTime = cityForecast.list[14].dt;
     forecast2Date.textContent = unixConversion(unixTime);
-    var day2IconCode = cityForecast.list[12].weather[0].icon;
+    var day2IconCode = cityForecast.list[14].weather[0].icon;
     var day2IconUrl = pngUrl + day2IconCode + '@2x.png';
     forecast2Symbol.src = day2IconUrl;
-    forecast2Temp.textContent = 'Temp: ' + cityForecast.list[12].main.temp + ' °F';
-    forecast2Wind.textContent = 'Wind: ' + cityForecast.list[12].wind.speed + ' MPH';
-    forecast2Humidity.textContent = 'Humidity: ' + cityForecast.list[12].main.humidity + ' %';
+    forecast2Temp.textContent = 'Temp: ' + cityForecast.list[14].main.temp + ' °F';
+    forecast2Wind.textContent = 'Wind: ' + cityForecast.list[14].wind.speed + ' MPH';
+    forecast2Humidity.textContent = 'Humidity: ' + cityForecast.list[14].main.humidity + ' %';
 
-    unixTime = cityForecast.list[20].dt;
+    unixTime = cityForecast.list[22].dt;
     forecast3Date.textContent = unixConversion(unixTime);
-    var day3IconCode = cityForecast.list[20].weather[0].icon;
+    var day3IconCode = cityForecast.list[22].weather[0].icon;
     var day3IconUrl = pngUrl + day3IconCode + '@2x.png';
     forecast3Symbol.src = day3IconUrl;
-    forecast3Temp.textContent = 'Temp: ' + cityForecast.list[20].main.temp + ' °F';
-    forecast3Wind.textContent = 'Wind: ' + cityForecast.list[20].wind.speed + ' MPH';
-    forecast3Humidity.textContent = 'Humidity: ' + cityForecast.list[20].main.humidity + ' %';
+    forecast3Temp.textContent = 'Temp: ' + cityForecast.list[22].main.temp + ' °F';
+    forecast3Wind.textContent = 'Wind: ' + cityForecast.list[22].wind.speed + ' MPH';
+    forecast3Humidity.textContent = 'Humidity: ' + cityForecast.list[22].main.humidity + ' %';
 
-    unixTime = cityForecast.list[28].dt;
+    unixTime = cityForecast.list[30].dt;
     forecast4Date.textContent = unixConversion(unixTime);
-    var day4IconCode = cityForecast.list[28].weather[0].icon;
+    var day4IconCode = cityForecast.list[30].weather[0].icon;
     var day4IconUrl = pngUrl + day4IconCode + '@2x.png';
     forecast4Symbol.src = day4IconUrl;
-    forecast4Temp.textContent = 'Temp: ' + cityForecast.list[28].main.temp + ' °F';
-    forecast4Wind.textContent = 'Wind: ' + cityForecast.list[28].wind.speed + ' MPH';
-    forecast4Humidity.textContent = 'Humidity: ' + cityForecast.list[28].main.humidity + ' %';
+    forecast4Temp.textContent = 'Temp: ' + cityForecast.list[30].main.temp + ' °F';
+    forecast4Wind.textContent = 'Wind: ' + cityForecast.list[30].wind.speed + ' MPH';
+    forecast4Humidity.textContent = 'Humidity: ' + cityForecast.list[30].main.humidity + ' %';
 
-    unixTime = cityForecast.list[36].dt;
+    unixTime = cityForecast.list[38].dt;
     forecast5Date.textContent = unixConversion(unixTime);
-    var day5IconCode = cityForecast.list[36].weather[0].icon;
+    var day5IconCode = cityForecast.list[38].weather[0].icon;
     var day5IconUrl = pngUrl + day5IconCode + '@2x.png';
     forecast5Symbol.src = day5IconUrl;
-    forecast5Temp.textContent = 'Temp: ' + cityForecast.list[36].main.temp + ' °F';
-    forecast5Wind.textContent = 'Wind: ' + cityForecast.list[36].wind.speed + ' MPH';
-    forecast5Humidity.textContent = 'Humidity: ' + cityForecast.list[36].main.humidity + ' %';
+    forecast5Temp.textContent = 'Temp: ' + cityForecast.list[38].main.temp + ' °F';
+    forecast5Wind.textContent = 'Wind: ' + cityForecast.list[38].wind.speed + ' MPH';
+    forecast5Humidity.textContent = 'Humidity: ' + cityForecast.list[38].main.humidity + ' %';
+    historyToList();
 }
 
 // Converts unix timestamp
 // This function is passed in throughout the rendering to convert the unix timestamp into
-// A date
+// a date
 function unixConversion(unixTime) {
     var date = new Date(unixTime * 1000);
 
